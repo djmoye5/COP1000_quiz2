cda40927d24fd0c76c83727e7083f9914bd224a0
Denerius Moye <dmoye@techlaunch.io>
2019-02-11 22:35:08 -0500

    first commit

---
+++ quiz2.js
@@ -0,0 +1,18 @@
+var queue = new Array();
+
+queue.push("Jeffery Gonzalez");
+queue.push("Amanda Smith");
+queue.push("Jessica Thomas");
+queue.push("Aida Medina");
+queue.push("Joe Nieves");
+queue.push("Justin Mincey");
+queue.push("Antonio Vargas");
+queue.push("Martin Johnson");
+queue.push("Michael Alvarez");
+queue.push("Jackson Briggs");
+queue.push("Sonya Cage");
+queue.push("Deion Williams");
+
+function nextUp(){
+    alert(queue.shift());
+}
